b8be5133b836500fe88ac6b007d3f3024dbe7748
Dan Hernandez <dan@Dans-MBP.lan1>
2019-09-24 20:55:20 -0400

    updated

--- index.ts
+++ index.ts
@@ -1,6 +1,3 @@
-import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
-import * as awsx from "@pulumi/awsx";
 
-//adding comment to kick off jenkins -updating for develop branch
-const s3Bucket = new aws.s3.Bucket("test-bucket");
+const s3Bucket = new aws.s3.Bucket("bucket");
